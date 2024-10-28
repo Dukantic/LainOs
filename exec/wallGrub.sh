4dfd59a9aab84c1cbc6220bfc2aa9a44b4747669
Dukantic <aubindo@outlook.fr>
2024-10-28 10:29:11 +0100

    creation fichier executable pour changer le wallpaper du grub + dossier avec les config externe + dossier  des different executable de notre creation

---
+++ exec/wallGrub.sh
@@ -0,0 +1,12 @@
+
+dossier_entree="$PWD/.."
+dossier_arrivee="/imageGRUB"
+image="copland.png"
+
+
+if [ ! -d "$dossier_arrivee" ]; then
+    mkdir -p "$dossier_arrivee"
+fi
+cp $dossier_entree/hypr/wallpaper/$image $dossier_arrivee
+cp $dossier_entree/otherConfig/grub /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
