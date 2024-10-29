168c449b937fbed08297dd3678ff5b24c6705b4d
Dukantic <aubindo@outlook.fr>
2024-10-29 10:39:31 +0100

    avencement nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -14,10 +14,20 @@ vim.opt.rtp:prepend(lazypath)
 
 
 require("lazy").setup({
-    -- LSP manager
+    {    -- LSP manager
 	"williamboman/mason.nvim",
 	"williamboman/mason-lspconfig.nvim",
 	"neovim/nvim-lspconfig",
+    config = function()
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = { "clangd" },
+        })
+
+        local lspconfig = require("lspconfig")
+        lspconfig.clangd.setup({})
+    end
+    },
 	-- Vscode-like pictograms
 	{
 		"onsails/lspkind.nvim",
@@ -46,6 +56,51 @@ require("lazy").setup({
         "scottmckendry/cyberdream.nvim",
         lazy = false,
         priority = 1000,
+        config = function()
+        require("cyberdream").setup({
+            transparent = true,
+            italic_comments = true,
+            hide_fillchars = false,
+            borderless_telescope = true,
+            terminal_colors = true,
+            cache = false,
+            theme = {
+                variant = "default",
+                saturation = 1,
+                highlights = {
+                    Comment = { fg = "#696969", bg = "NONE", italic = true },
+                },
+                overrides = function(colors)
+                    return {
+                        Comment = { fg = colors.green, bg = "NONE", italic = true },
+                        ["@property"] = { fg = colors.magenta, bold = true },
+                    }
+                end,
+                colors = {
+                     bg = "#1a1a1a",
+                    green = "#00ff00",
+                    magenta = "#ff00ff",
+                },
+            },
+            extensions = {
+                telescope = true,
+                notify = true,
+                mini = true,
+            },
+        })
+    end
+    },
+    {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = { "c" },
+            highlight = {
+                enable = true,
+            },
+        })
+    end
     }
 })
 
