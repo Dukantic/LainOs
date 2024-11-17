a794e4439ce3af9a51836e1e37f33e56372f2d1a
Dukantic <aubindo@outlook.fr>
2024-11-17 12:28:41 +0100

    change color waybar, change sddm theme and bash for sddm.conf

--- exec/themeRoot.sh
+++ exec/themeRoot.sh
@@ -13,5 +13,13 @@ sudo cp $dossier_entree/otherConfig/grub /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 
-theme_sddm="/usr/share/sddm/themes/"
-sudo cp -r "$parent_dir/otherConfig/simple-lain-sddm/" "$theme_sddm/simple-lain-sddm"
+theme_sddm="/usr/share/sddm/themes/simple-lain-sddm"
+if [ -d "$theme_sdm"]; then
+  sudo rm -drf "$theme_sddm.old"
+  sudo mv -f "$theme_sddm" "$theme_sddm.old"
+fi
+sudo cp -r "$parent_dir/otherConfig/simple-lain-sddm" "$theme_sddm"
+
+conf_sddm="/etc/sddm.conf"
+sudo rm -drf "$conf_sddm"
+sudo cp "$parent_dir/otherConfig/sddm.conf" "conf_sddm"
