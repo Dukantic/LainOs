397f92938706d7f37bf4c601b3b8e4ee01702f67
Dukantic <aubindo@outlook.fr>
2024-11-19 12:12:47 +0100

    change bash for link and theme nvim

--- exec/linkOnConfig.sh
+++ exec/linkOnConfig.sh
@@ -6,15 +6,14 @@
 parent_dir=$(dirname "$(dirname "$(realpath "$0")")")
 link_path="$HOME/.bashrc"
 
-if [ -f "$link_path" ] && [ ! -L "$link_path" ]; then
-    mv "$link_path" "$HOME/.BASHRC_OLD"
-    echo "creation .BASHRC_OLD."
+if [ -f "$link_path" ]; then
+    rm "$link_path.OLD"
+    mv -f "$link_path" "$link_path.OLD"
+    echo "creation .bashrc.OLD."
 
 fi
-if [ ! -L "$link_path" ]; then
-    ln -s "$parent_dir/.bashrc" "$link_path"
-    echo ".clang-format done."
-fi
+ln -s "$parent_dir/.bashrc" "$link_path"
+echo ".bashrc done."
 
 
 # .clang-format -> .config/otherConfig/.clang-format
@@ -22,9 +21,10 @@ fi
 parent_dir=$(dirname "$(dirname "$(realpath "$0")")")
 link_path="$HOME/.clang-format"
 
-if [ -f "$link_path" ] && [ ! -L "$link_path" ]; then
-    mv "$link_path" "$HOME/.CLANG_FORMAT_OLD"
-    echo "creation .CLANG_FORMAT_OLD."
+if [ -f "$link_path" ]; then
+    rm "$link_path.OLD"
+    mv -f "$link_path" "$link_path.OLD"
+    echo "creation .CLANG_FORMAT.OLD."
 fi
 
 if [ ! -L "$link_path" ]; then
